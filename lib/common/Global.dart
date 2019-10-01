3c432554a376d7f4492e093ed89f6feec0261928
SeekerGAO <1532584997@qq.com>
2019-10-01 23:45:29 +0800

    新建全局变量Global类

---
+++ lib/common/Global.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// 创建五套主题
+const _theme = <MaterialColor>[
+  Colors.blue,
+  Colors.green,
+  Colors.teal,
+  Colors.orange,
+  Colors.grey
+]
+
+class Global{
+
+}
